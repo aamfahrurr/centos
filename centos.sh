761a36797981b1b44018348aebd1b99456555224
aamfahrurr <aam.fahrur@ti.stmikdb.ac.id>
2017-05-01 02:18:11 +0700

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -17,8 +17,8 @@ yum -y install wget
 wget http://prdownloads.sourceforge.net/webadmin/webmin-1.831-1.noarch.rpm
 yum -y install perl perl-Net-SSLeay openssl perl-IO-Tty
 rpm -U webmin-1.831-1.noarch.rpm
-
 sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
+service webmin restart
 
 # install dropbear
 yum -y update
